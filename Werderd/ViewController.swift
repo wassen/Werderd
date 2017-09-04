2ffa499be066bc521b3a9cd5a4c3e2fe971caf55
wassen <k-asayama@outlook.com>
2017-09-05 00:02:10 +0900

    first commit

---
+++ Werderd/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Werderd
+//
+//  Created by 浅山和宣 on 2017/08/18.
+//  Copyright © 2017年 Kazuki Asayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var senibutton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
