491f560588dc30d42dacfd59bedad395b615cc49
wassen <k-asayama@outlook.com>
2018-02-28 00:11:58 +0900

    delete Main.storyboard

--- Werderd/ViewController.swift
+++ Werderd/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
     }
 
     override func didReceiveMemoryWarning() {
