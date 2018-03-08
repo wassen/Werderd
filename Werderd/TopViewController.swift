2e2e7e8eeec1eb1f4956615375fccee6dd23fe7e
wassen <k-asayama@outlook.com>
2018-03-08 23:57:47 +0900

    feat: try displaying color cells for experiment

--- Werderd/TopViewController.swift
+++ Werderd/TopViewController.swift
@@ -9,33 +9,72 @@
 import UIKit
 
 class TopViewController: UICollectionViewController {
-
     override func loadView() {
         self.view = UIView()
-
         self.navigationItem.title = "title"
+        self.collectionView = self.newCollectionView()
 
-        self.collectionView?.backgroundColor = .blue
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+//        collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+//        collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+//        collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+    }
 
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 100)
-        let collectionView = UICollectionView(frame: CGRect(), collectionViewLayout: layout)
-        collectionView.backgroundColor = .red
-
-        self.view.addSubview(collectionView)
-
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.delegate = self
+        self.collectionView!.dataSource = self
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func collectionView(
+        _ collectionView              : UICollectionView,
+        numberOfItemsInSection section: Int
+        ) -> Int {
+        return 3
+    }
 
-
+    override func collectionView(
+        _ collectionView       : UICollectionView,
+        cellForItemAt indexPath: IndexPath
+        ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "Cell",
+            for                : indexPath
+        )
+        
+        let colors = [
+            UIColor.red,
+            UIColor.blue,
+            UIColor.green,
+            UIColor.yellow
+        ]
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
 }
 
+// MARK: Private Method for loadView
+extension TopViewController {
+    private func newCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(
+            width : 100,
+            height: 100
+        )
+
+        let collectionView = UICollectionView(
+            frame               : CGRect(),
+            collectionViewLayout: layout
+        )
+        collectionView.backgroundColor = .black
+
+        return collectionView
+    }
+}
