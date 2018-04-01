3d4a545f14d6bf2418b967c8fec796a1f5091006
kazuki-asayama-d <kazuki_asayama@dwango.co.jp>
2018-04-02 00:05:39 +0900

    feat: change layout

--- Werderd/ViewControllers/AddWordViewController.swift
+++ Werderd/ViewControllers/AddWordViewController.swift
@@ -11,6 +11,8 @@ import Cartography
 
 class AddWordViewController: UIViewController {
     var okButton: UIButton!
+    var text: String?
+    var textField: UITextField!
 
     override func loadView(){
         self.view = UIView()
@@ -26,26 +28,33 @@ class AddWordViewController: UIViewController {
 
         self.view.addSubview(okButton)
 
-        let textField = UITextField()
+        self.textField = UITextField()
         textField.backgroundColor = .base
         self.view.addSubview(textField)
 
         constrain(self.okButton, textField, self.view) { okButton, textField, view in
 //            okButton.width   == 100
 //            okButton.height  == 100
-            textField.centerX == okButton.centerX
-            textField.centerY == okButton.centerY
-            okButton.centerX == view.centerX
-            okButton.centerY == view.centerY
+            textField.centerX == view.centerX
+            textField.centerY == view.centerY / 2
+            textField.width   == view.width / 4
+            okButton.centerX  == view.centerX
+            okButton.centerY  == view.centerY
         }
 
         self.view.backgroundColor = .white
 
         self.navigationItem.title = "New word"
     }
+    
+    override func viewDidLoad() {
+//        self.textField.delegate = self
+    }
 
     @objc func tapped() {
-        WerderdTypeModel.default.types.append("appended")
+        if let text = self.textField.text {
+            WerderdTypeModel.default.types.append(text)
+        }
         self.dismiss(animated: true)
     }
 }
